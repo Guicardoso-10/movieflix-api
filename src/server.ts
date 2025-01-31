59e357cb32f2369ce33c3721bba1c491c95a3fdd
Guilherme Cardoso <guiguicardoso-10@hotmail.com>
2025-01-31 10:16:12 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -22,9 +22,22 @@ app.get("/movies", async (_, res) => {
 })
 
 app.post("/movies", async (req, res) => {
+    
     const { title, genre_id, lang_id, release_date, oscar_count } = req.body
     
-    try {
+    try{
+
+        //verificar se o filme que será cadastrado já não existe no banco de dados
+        const movieWithTheSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: {equals: title, mode: "insensitive"}
+            }
+        })
+
+        if (movieWithTheSameTitle) {
+            return res.status(409).send({message: "Já existe um filme cadastrado com esse título"})
+        }
+
         await prisma.movie.create({
             data: {
                 title: title,
@@ -34,7 +47,7 @@ app.post("/movies", async (req, res) => {
                 release_date: new Date(release_date)
             }
         })
-    } catch (error) {
+    }catch(error){
         return res.status(500).send({message: "Falha ao cadastrar um filme"})
     }
 
